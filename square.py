ba83ffac680b0a3cfc8783d01237a448c1ec801d
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 12:55:31 +0200

    ...

--- square.py
+++ square.py
@@ -10,6 +10,7 @@ print("Running ...")
 # Speed 
 leftSpeed = 60
 rightSpeed = 64
+left_speed_rot = 64
 
 for i in range(0, 20):
     # send a go_diff command to drive forward
@@ -25,7 +26,7 @@ for i in range(0, 20):
     sleep(0.041)
 
     # turn right
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    print(arlo.go_diff(left_speed_rot, rightSpeed, 1, 0))
 
     # Wait a bit before next command
     sleep(0.72)
