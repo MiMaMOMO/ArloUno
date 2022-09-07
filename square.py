c7215ea3b373aa9256a832e32139f1a0519b8f50
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 12:51:36 +0200

    ...

--- square.py
+++ square.py
@@ -28,7 +28,7 @@ for i in range(0, 20):
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
 
     # Wait a bit before next command
-    sleep(0.7)
+    sleep(0.72)
 
     # send a stop command
     print(arlo.stop())
