f6412e9dd9b380cde1bd8529fd9ba7a314ecde64
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 14:09:20 +0200

    ...

--- eight.py
+++ eight.py
@@ -12,8 +12,8 @@ print("Running ...")
 # Speed 
 leftSpeed = 64
 rightSpeed = 64
-rot_speed_right = 32
-rot_speed_left = 32
+rot_speed_right = 0
+rot_speed_left = 0
 
 for i in range(0, 20):
 
@@ -30,7 +30,7 @@ for i in range(0, 20):
     print(arlo.go_diff(leftSpeed, rot_speed_right, 1, 1))
 
     # Wait a bit before next command
-    sleep(8)
+    sleep(5)
     
     # Wait a bit before next command
     sleep(0.041)
@@ -48,7 +48,7 @@ for i in range(0, 20):
     print(arlo.go_diff(rot_speed_left,  rightSpeed, 1, 1))
 
     # Wait a bit before next command
-    sleep(8)
+    sleep(5)
     
     # Wait a bit before next command
     sleep(0.041)
