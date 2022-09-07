feb949e83a5745bd441097ac16ca413da1e8d87f
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 12:26:24 +0200

    ...

--- former.py
+++ former.py
@@ -8,11 +8,18 @@ arlo = robot.Robot()
 print("Running ...")
 
 # Speed 
-leftSpeed = 59.7
+leftSpeed = 60
 rightSpeed = 64
 
 # send a go_diff command to drive forward
 print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+sleep(10)
+
+print(arlo.stop())
+
+# send a go_diff command to drive forward
+print(arlo.go_diff(leftSpeed, rightSpeed, 0, 0))
 
 sleep(10)
 
