6e90a5e7dfe4388a39f53ce21ccedb08161dc7c3
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 12:05:32 +0200

    ...

---
+++ square.py
@@ -0,0 +1,32 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+
+# send a go_diff command to drive forward
+leftSpeed = 64
+rightSpeed = 64
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+# Wait a bit while robot moves forward
+sleep(3)
+
+# send a stop command
+print(arlo.stop())
+
+# Wait a bit before next command
+sleep(0.041)
+
+# turn right
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+
+# Wait a bit before next command
+sleep(1)
+
+# send a stop command
+print(arlo.stop())
