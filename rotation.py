afaa26b073377518a51cc708bc65937650101e30
Mohamad Chleih <Chleih@outlook.com>
2022-09-07 13:00:38 +0200

    ...

---
+++ rotation.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+print("Running ...")
+
+# Speed 
+leftSpeed = 60
+rightSpeed = 64
+
+# send a go_diff command to drive forward
+print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+
+sleep(3)
+
+print(arlo.stop())
