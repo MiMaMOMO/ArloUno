91da6b77a3bebd4db50cd4cb0a57403d2b6abf32
Mohamad Chleih <Chleih@outlook.com>
2022-09-12 15:03:14 +0200

    ...

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -1,4 +1,4 @@
-# Testing settings for Arlo for 1 meter 
+# A2.1 - Obstacle avoidance 
 
 import robot
 
@@ -18,7 +18,9 @@ for i in range(0, 100):
     
     front_distance = arlo.read_front_ping_sensor() / 1000
     
-    if front_distance < 1: 
+    print(front_distance)
+    
+    if front_distance <= 1: 
         # Send a go_diff command to drive forward.
         # Rotate right by only driving with the left wheel
         print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
@@ -28,8 +30,6 @@ for i in range(0, 100):
     # send a go_diff command to drive forward
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
-    # The time the robot have to drive 1 meter 
-    sleep(3)
 
-    # Stop the robot 
-    print(arlo.stop())
+# Stop Arlo 
+print(arlo.stop())
