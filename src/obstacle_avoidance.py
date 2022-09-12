d2a1b7790f591ad740670640b0cdc8dafab6a29c
Mohamad Chleih <Chleih@outlook.com>
2022-09-12 15:15:30 +0200

    ...

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -12,10 +12,14 @@ print("Running ...")
 # Robot speed. We use different speed here 
 leftSpeed = 60
 rightSpeed = 64
+rot_speed_front = 1.44
+rot_speed_sides = 0.728
 
 for i in range(0, 10000):
     
     front_distance = arlo.read_front_ping_sensor()
+    left_distance = arlo.read_left_ping_sensor()
+    right_distance = arlo.read_left_ping_sensor()
     
     print(front_distance)
     
@@ -24,11 +28,24 @@ for i in range(0, 10000):
         # Rotate right by only driving with the left wheel
         print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
         
-        sleep(0.728)
+        sleep(rot_speed_front)
+        
+    if left_distance <= 200:
+        # Send a go_diff command to drive forward.
+        # Rotate right by only driving with the left wheel
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+        
+        sleep(rot_speed_sides)
+        
+    if right_distance <= 200:
+        # Send a go_diff command to drive forward.
+        # Rotate right by only driving with the left wheel
+        print(arlo.go_diff(leftSpeed, rightSpeed, 0, 1))
+        
+        sleep(rot_speed_sides)
     
     # send a go_diff command to drive forward
     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
 
 # Stop Arlo 
 print(arlo.stop())
