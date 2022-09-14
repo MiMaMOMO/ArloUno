9f2d7935d775de714a4a2e19fbf64b45d6a0dc1e
Mohamad Chleih <Chleih@outlook.com>
2022-09-14 11:02:21 +0200

    ...

--- src/sonar_calibration.py
+++ src/sonar_calibration.py
@@ -19,7 +19,7 @@ rot_speed_sides = 0.728
 
 
 # Run Arlo 
-for i in range(0, 10000):
+while 1:
     
     front_distance = arlo.read_front_ping_sensor()
     # left_distance = arlo.read_left_ping_sensor()
@@ -27,15 +27,15 @@ for i in range(0, 10000):
     
     print(front_distance)
     
-    if front_distance <= 200: 
-        # Send a go_diff command to drive forward.
-        # Rotate right by only driving with the left wheel
-        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
+    # if front_distance <= 200: 
+    #     # Send a go_diff command to drive forward.
+    #     # Rotate right by only driving with the left wheel
+    #     print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
         
-        sleep(rot_speed_front)
+    #     sleep(rot_speed_front)
     
     # send a go_diff command to drive forward
-    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+    # print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
 
 # Stop Arlo 
 print(arlo.stop())
