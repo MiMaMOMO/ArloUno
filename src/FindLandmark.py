ae1a34d4bd3bc2d72f4379cf7bcbaa1fae74230d
Mikkel <Mikkel.luc.carlsen@boag.nu>
2022-09-28 11:22:14 +0200

    t

--- src/FindLandmark.py
+++ src/FindLandmark.py
@@ -32,36 +32,49 @@ WIN_RF = "Example 1"
 cv2.namedWindow(WIN_RF)
 cv2.moveWindow(WIN_RF, 100, 100)
 
-retval, frameReference = cam.read() # Read frame
-    
-if not retval: # Error
-    print(" < < <  Game over!  > > > ")
-    exit(-1)
-
-# Show frames
-
-
-arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 arucoParams = cv2.aruco.DetectorParameters_create()
-corners, ids, rejected = cv2.aruco.detectMarkers(frameReference, arucoDict, parameters=arucoParams)
 
-cv2.imshow(WIN_RF, frameReference)
+while cv2.waitKey(4) == -1:
+    retval, frameReference = cam.read() # Read frame
+        
+    if not retval: # Error
+        print(" < < <  Game over!  > > > ")
+        exit(-1)
 
-print(f"corners: {corners}")
-print(f"ids: {ids}")
-#print(f"rejected: {rejected}")
+    corners, ids, rejected = cv2.aruco.detectMarkers(frameReference, arucoDict, parameters=arucoParams)
 
-# # Create a robot object and initialize
-# arlo = robot.Robot()
-# print("Running ...")
-# # Arlo speed. We use the same speed here 
-# leftSpeed = 64
-# rightSpeed = 64
-# # Send a go_diff command to drive forward.
-# print(arlo.go_diff(leftSpeed, rightSpeed, 1, 0))
-# # The time Arlo have to rotate 90 deegrees before the next command 
-# sleep(0.728)
-# # Stop Arlo 
-# print(arlo.stop())
-# # Wait a bit before next command
-# sleep(0.041)
+    # verify *at least* one ArUco marker was detected
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            # extract the marker corners (which are always returned in
+            # top-left, top-right, bottom-right, and bottom-left order)
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+    # draw the bounding box of the ArUCo detection
+    cv2.line(frameReference, topLeft, topRight, (0, 255, 0), 2)
+    cv2.line(frameReference, topRight, bottomRight, (0, 255, 0), 2)
+    cv2.line(frameReference, bottomRight, bottomLeft, (0, 255, 0), 2)
+    cv2.line(frameReference, bottomLeft, topLeft, (0, 255, 0), 2)
+    # compute and draw the center (x, y)-coordinates of the ArUco
+    # marker
+    cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+    cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+    cv2.circle(frameReference, (cX, cY), 4, (0, 0, 255), -1)
+    # draw the ArUco marker ID on the frameReference
+    cv2.putText(frameReference, str(markerID),
+        (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+        0.5, (0, 255, 0), 2)
+    print("[INFO] ArUco marker ID: {}".format(markerID))
+    # show the output frameReference
+    cv2.imshow("frameReference", frameReference)
+
