958b2c81ab3369e7aa3681c2a1eced94069c1f90
Mikkel <Mikkel.luc.carlsen@boag.nu>
2022-09-28 11:07:56 +0200

    t

--- src/FindLandmark.py
+++ src/FindLandmark.py
@@ -45,6 +45,7 @@ arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
 arucoParams = cv2.aruco.DetectorParameters_create()
 corners, ids, rejected = cv2.aruco.detectMarkers(frameReference, arucoDict, parameters=arucoParams)
 
+print(f"corners: {corners}")
 
 # # Create a robot object and initialize
 # arlo = robot.Robot()
