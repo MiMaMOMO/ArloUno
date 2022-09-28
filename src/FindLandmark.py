6ec05910d1996600a04f87454dd46b759284c990
Mikkel <Mikkel.luc.carlsen@boag.nu>
2022-09-28 15:52:59 +0200

    t

--- src/FindLandmark.py
+++ src/FindLandmark.py
@@ -59,7 +59,7 @@ while cv2.waitKey(4) == -1:
             right = 1 - sign
             print(arlo.go_diff(64, 64, left, right))#right turn
             sleep(np.abs(np.degrees(beta)) * 0.008)
-            #print(arlo.stop())
+            print(arlo.stop())
             #sleep(1.0)
 
 
