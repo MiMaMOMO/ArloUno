73dd33810ed543bb1b7c61314d4422406f2a306b
Mikkel <Mikkel.luc.carlsen@boag.nu>
2022-10-03 15:08:50 +0200

    t

--- src/FindLandmark.py
+++ src/FindLandmark.py
@@ -41,7 +41,7 @@ distCoeffs = np.asarray([0,0,0,0])
 
 while cv2.waitKey(4) == -1:
 
-    for i in range(7):
+    for i in range(9):
         retval, frameReference = cam.read() # Read frame
 
     if not retval: # Error
