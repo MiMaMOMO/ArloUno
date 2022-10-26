72992d31673b98446723e9c977425a881bd012b5
Mohamad Chleih <Chleih@outlook.com>
2022-10-26 10:57:52 +0200

    ...

--- src/commands.py
+++ src/commands.py
@@ -47,19 +47,18 @@ def drive(dist, landmark_range = 0.0) -> None:
 
     # Make Arlo drive forward 
     arlo.go_diff(LEFT_VELOCITY, RIGHT_VELOCITY, 1, 1) 
-    #time.sleep(0.01)
+    time.sleep(0.01)
     
     # Control what happens while Arlo drives with the program 
     # If the amound of time have passed, stop Arlo 
-    #while 1:
+    while 1:
 
         # If Arlo wants to go near a box we account for that by a certain tolerance 
         # Otherwise let Arlo drive the full dist
-    #if timer.elapsed_time() > (drive_time - (METER * landmark_range)):
-    if timer.elapsed_time() > 3:
-        print("Stop!")
-        arlo.stop()
-        #break   
+        if timer.elapsed_time() > (drive_time - (METER * landmark_range)):
+            print("Stop!")
+            arlo.stop()
+            #break   
         
 drive(200)  
 print("Drived!")  
