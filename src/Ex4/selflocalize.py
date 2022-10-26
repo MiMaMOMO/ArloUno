34ad461b1058ea2c4aafb4275910c93308d40528
Mohamad Chleih <Chleih@outlook.com>
2022-10-26 14:53:36 +0200

    ...

--- src/Ex4/selflocalize.py
+++ src/Ex4/selflocalize.py
@@ -60,8 +60,8 @@ velocity = 0.0              # cm/sec
 angular_velocity = 0.0      # radians/sec
 
 # Spread parameters  
-spread_dist = 20.0          # The spread for the distance 
-spread_angle = 1.0          # The spread for the orientation 
+spread_dist = 25.0          # The spread for the distance 
+spread_angle = 2.0          # The spread for the orientation 
 
 
 def jet(x):
