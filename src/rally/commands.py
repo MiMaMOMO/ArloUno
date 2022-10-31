16f18342bb565725fec43008c7e4dd0be08a176f
Mohamad Chleih <Chleih@outlook.com>
2022-10-31 14:01:23 +0100

    ...

--- src/rally/commands.py
+++ src/rally/commands.py
@@ -1,7 +1,7 @@
 import numpy as np 
-from rally.timer import Timer
 
-from rally.settings import * 
+from rally.timer import Timer
+from settings import * 
 
 
 def rotate(arlo, angle) -> None:
@@ -16,7 +16,7 @@ def rotate(arlo, angle) -> None:
     sign = np.sign(angle)                           # Get the sign of the angle 
     scaled_angle = np.abs(np.degrees(angle))        # Make radians into degrees. Get absolute value
     rot_time = scaled_angle * (ORIENTATION / 90)    # Seconds it takes Arlo to rotate angle amount 
-    timer = Timer()                                 # Timer to measure a countdown for the rotation
+    t = Timer()                                     # Timer to measure a countdown for the rotation
     
     # TODO: Try to initialize the timer after giving the go command to arlo
     
@@ -28,14 +28,14 @@ def rotate(arlo, angle) -> None:
     
     # Make Arlo rotate in the right direction 
     arlo.go_diff(LEFT_ROT_VELOCITY, RIGHT_ROT_VELOCITY, left_dir, right_dir) 
-    timer.sleep(0.01)
+    t.sleep(0.01)
     
     while 1: 
         #print("Real time    : {}".format(time.perf_counter()))
         #print("Elapsed time : {}".format(timer.elapsed_time()))
 
         # Break when Arlo have spent the seconds needed to perform the rotation 
-        if timer.elapsed_time() > rot_time:
+        if t.elapsed_time() > rot_time:
             arlo.stop()
             break
 
@@ -51,13 +51,13 @@ def drive(arlo, dist, landmark_range = 0.0) -> None:
 
     scaled_dist = dist / 100                    # Scale the distance down to meters  
     drive_time = scaled_dist * METER            # How long in seconds it takes Arlo to drive dist
-    timer = timer.Timer()                             # Timer used to measure a countdown for Arlo
+    t = Timer()                                 # Timer used to measure a countdown for Arlo
 
     # TODO: Try to initialize the timer after giving the go command to arlo  
 
     # Make Arlo drive forward 
     arlo.go_diff(LEFT_VELOCITY, RIGHT_VELOCITY, 1, 1) 
-    timer.sleep(0.01)
+    t.sleep(0.01)
     
     # Control what happens while Arlo drives with the program 
     # If the amount of time have passed, stop Arlo 
@@ -67,7 +67,7 @@ def drive(arlo, dist, landmark_range = 0.0) -> None:
 
         # If Arlo wants to go near a box we account for that by a certain tolerance 
         # Otherwise let Arlo drive the full dist
-        if timer.elapsed_time() > (drive_time - (METER * landmark_range)):
+        if t.elapsed_time() > (drive_time - (METER * landmark_range)):
             arlo.stop()
             break    
         
