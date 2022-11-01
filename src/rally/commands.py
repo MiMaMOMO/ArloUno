e2cc9dc06caa913ae6260eaf4917ef505d1ffd57
Mohamad Chleih <Chleih@outlook.com>
2022-11-01 16:08:10 +0100

    ...

--- src/rally/commands.py
+++ src/rally/commands.py
@@ -120,7 +120,7 @@ def scan(arlo, cam, landmark = None):
     
     # TODO: Numpy this 
     # Rotate a full turn until we find some Aruco landmarks 
-    for _ in range(36):
+    for _ in range(136):
         rotate(arlo, DEGREES_10)
         detected = detect(cam)
         
