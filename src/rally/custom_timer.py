03cd894c1ed55383e2f41eaf84e8d70ffd17c113
Mohamad Chleih <Chleih@outlook.com>
2022-11-02 14:20:27 +0100

    ...

--- src/rally/custom_timer.py
+++ src/rally/custom_timer.py
@@ -18,7 +18,7 @@ class Timer:
         # Return the elapsed time between intialization and now 
         return (time.perf_counter() - self.start_time)
     
-    def sleep(self, val) -> None:
+    def sleep(val) -> None:
         '''
         Sleeps a certain amount of time. 
         '''
