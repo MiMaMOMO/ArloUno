202c871d127a0cbc81e5c2ec5779875fb3579ab1
Mohamad Chleih <Chleih@outlook.com>
2022-11-04 18:07:05 +0100

    ...

--- src/rally/drive_test.py
+++ src/rally/drive_test.py
@@ -1,6 +1,18 @@
-import robot
+import sys
 
 import commands
+from settings import * 
+
+if ON_ROBOT:
+    sys.path.append("../robot")
+    
+# Try to import robot module
+try:
+    import robot
+    ON_ROBOT = True
+except ImportError:
+    print("selflocalize.py: robot module not present - forcing not running on Arlo!")
+    ON_ROBOT = False
 
 # Create a robot object and initialize
 arlo = robot.Robot()
