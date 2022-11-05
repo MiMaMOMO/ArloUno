3b8674be97899c61f501fcc3c078bee0f70e8961
Mohamad Chleih <Chleih@outlook.com>
2022-11-05 12:51:46 +0100

    ...

--- src/tests/cam_test.py
+++ src/tests/cam_test.py
@@ -26,50 +26,47 @@ def get_cam():
 
 
 ### MAIN PROGRAM ###
-try:
+# Open windows
+open_windows()
 
-    # Open windows
-    open_windows()
+# Initialize Arlo
+arlo = robot.Robot()
 
-    # Initialize Arlo
-    arlo = robot.Robot()
+print("Opening and initializing camera")
 
-    print("Opening and initializing camera")
+# Check which camera we want to use
+cam = get_cam()
 
-    # Check which camera we want to use
-    cam = get_cam()
+# Try to selflocalize and get to the center point using the particle filter
+while 1:
 
-    # Try to selflocalize and get to the center point using the particle filter
-    while 1:
+    # print("x: {}".format(est_pose.getX()))
+    # print("y: {}".format(est_pose.getY()))
+    # print("t: {}".format(est_pose.getTheta()))
 
-        # print("x: {}".format(est_pose.getX()))
-        # print("y: {}".format(est_pose.getY()))
-        # print("t: {}".format(est_pose.getTheta()))
+    # Get a pressed key if any for 10 ms. Maybe if removed could boost performance?
+    action = cv2.waitKey(10)
 
-        # Get a pressed key if any for 10 ms. Maybe if removed could boost performance?
-        action = cv2.waitKey(10)
+    # Quit if we press q
+    if action == ord('q'):
+        break
+    
+    # Fetch next frame
+    frame = cam.get_next_frame()
 
-        # Quit if we press q
-        if action == ord('q'):
-            break
-        
-        # Fetch next frame
-        frame = cam.get_next_frame()
+    # Detect objects
+    objectIDs, dists, angles = cam.detect_aruco_objects(frame)
 
-        # Detect objects
-        objectIDs, dists, angles = cam.detect_aruco_objects(frame)
+    # We detected atleast one landmark
+    if not isinstance(objectIDs, type(None)):
 
-        # We detected atleast one landmark
-        if not isinstance(objectIDs, type(None)):
+        # List detected objects
+        for i in range(len(objectIDs)):
+            print(
+                "Object ID = ", objectIDs[i], ", Distance = ", dists[i], ", angle = ", angles[i])
 
-            # List detected objects
-            for i in range(len(objectIDs)):
-                print(
-                    "Object ID = ", objectIDs[i], ", Distance = ", dists[i], ", angle = ", angles[i])
-
-            # Draw detected objects
-            cam.draw_aruco_objects(frame)
+        # Draw detected objects
+        cam.draw_aruco_objects(frame)
 
 # Make sure to clean up even if an exception occurred
-finally:
-    clean_up(cam)
+clean_up(cam)
