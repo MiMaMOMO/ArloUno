cf1594ca9b1dbaded74dc811c02ebaac50480124
Mohamad Chleih <Chleih@outlook.com>
2022-11-06 16:04:58 +0100

    ...

--- src/rally/brute.py
+++ src/rally/brute.py
@@ -191,7 +191,6 @@ def run_brute() -> None:
 
         # Arlo found its way to the landmark so we wanna look for the next landmark 
         rute_idx += 1
-        break
 
     # Stop Arlo just in case he continues after endning the rute 
-    arlo.stop()
+    # arlo.stop()
